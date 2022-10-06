f9af105fc18578b56d23c269b6d896b45522d06a
Andres Vallecillo <andresmvallecillo@gmail.com>
2022-10-05 21:42:35 -0300

    actividad 4

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,19 @@
 import "./ItemListContainer.css";
 import Title from "../Title/Title";
+import ItemCount from "../ItemCount";
+
 
 const ItemListContainer = ()=>{
+
+    const onAdd=(cantidad)=>{
+        console.log(`Compraste ${cantidad} unidades`)
+    }
+
     return (
+        <>
         <Title greeting='Bienvenidos a Andrelos Bar' />
+        <ItemCount initial={1} stock={10} onAdd={onAdd}  />
+        </>
     );
 }
 
